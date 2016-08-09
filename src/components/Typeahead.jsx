2c708415349b8365af6d8ca9f74e877719720086
ryanirilli <ryanirilli@gmail.com>
2016-08-08 21:31:58 -0700

    update typeahead

--- src/components/Typeahead.jsx
+++ src/components/Typeahead.jsx
@@ -13,18 +13,67 @@ export default React.createClass({
 
   getInitialState() {
     return {
+      isShowingResults: true,
+      curActiveIndex: null,
       input: '',
       fetchData: debounce(this.fetchData, 250)
     }
   },
 
+  onKeydown(event) {
+    const { keyCode } = event;
+    switch(keyCode) {
+      case 27:
+        //esc
+        this.setState({ isShowingResults: false, curActiveIndex: null });
+        break;
+      case 38:
+        //arrow up
+        this.setCurrentActiveIndex('up');
+        break;
+      case 40:
+        //arrow down
+        this.setCurrentActiveIndex('down');
+        break;
+    }
+  },
+
+  setCurrentActiveIndex(direction) {
+    const { results } = this.props;
+    let { curActiveIndex } = this.state;
+
+    if(!results.size) {
+      return;
+    }
+
+    if (curActiveIndex === null || (curActiveIndex === 0 && direction === 'up')) {
+      curActiveIndex = 0;
+    } else if(curActiveIndex !== results.size - 1) {
+      switch (direction) {
+        case 'up':
+          curActiveIndex--;
+          break;
+        case 'down':
+          curActiveIndex++;
+          break;
+      }
+    }
+    this.setState({ curActiveIndex });
+  },
+
+  setCurrentActiveIndexOnHover(index) {
+    this.setState({ curActiveIndex: index });
+  },
+
   render() {
-    return <div className={`typeahead typeahead--${this.props.theme || 'main'} ${this.props.results.size ? 'typeahead--has-results' : ''}`}>
+    const shouldShowResults = this.props.results.size && this.state.isShowingResults;
+    return <div ref="typeahead" className={`typeahead typeahead--${this.props.theme || 'main'} ${shouldShowResults ? 'typeahead--has-results' : ''}`}
+                onKeyDown={this.onKeydown}>
       <input className="typeahead__input u-1/1"
              type="text"
              value={this.state.input}
              onChange={this.handleInput} />
-      {this.props.results.size ? this.renderResults() : null}
+      {shouldShowResults ? this.renderResults() : null}
     </div>
   },
   
@@ -35,13 +84,17 @@ export default React.createClass({
   },
   
   renderResult(result, i) {
-    return <li key={result.get('id')} className="typeahead__result">
+    const { curActiveIndex } = this.state;
+    return <li key={result.get('id')}
+               onMouseEnter={() => this.setCurrentActiveIndexOnHover(i)}
+               className={`typeahead__result ${curActiveIndex === i ? 'typeahead__result--active' : ''}`}>
       {this.props.renderResult(result, i)}
     </li>
   },
 
   fetchData() {
     const { input } = this.state;
+    this.setState({ isShowingResults: true, curActiveIndex: null });
     this.props.fetchData(input);
   },
 
